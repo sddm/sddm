9afd9f8ba4d0c5ca063adcb015d61d44c86624da
Abdurrahman AVCI <abdurrahmanavci@gmail.com>
2013-01-25 01:07:35 +0000

    Add a new lock file class.

---
+++ src/LockFile.cpp
@@ -0,0 +1,89 @@
+/***************************************************************************
+* Copyright (c) 2013 Abdurrahman AVCI <abdurrahmanavci@gmail.com
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the
+* Free Software Foundation, Inc.,
+* 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+***************************************************************************/
+
+#include "LockFile.h"
+
+#include <QDebug>
+#include <QFile>
+
+#include <fstream>
+
+#include <unistd.h>
+#include <signal.h>
+
+namespace SDE {
+    class LockFilePrivate {
+    public:
+        QString path { "" };
+        bool testing { false };
+
+        bool success { true };
+        QString errorMessage { "" };
+    };
+
+    LockFile::LockFile(const QString &path, bool testing) : d(new LockFilePrivate) {
+        d->path = path;
+        d->testing = testing;
+        // if testing, return
+        if (d->testing)
+            return;
+        // check if lock file exists
+        if (QFile::exists(path)) {
+            // if exists, read pid from it
+            std::ifstream test(path.toStdString());
+            pid_t pid = 0;
+            test >> pid;
+            test.close();
+            // check if another process with the pid is running
+            if (pid > 0) {
+                int ret = kill(pid, 0);
+                if (ret == 0 || errno == EPERM) {
+                    qCritical() << "error: another instance of the program is running with PID" << pid << ".";
+                    d->success = false;
+                    return;
+                }
+            }
+            // remove lock file
+            QFile::remove(path);
+        }
+        // create lock
+        std::ofstream lock(path.toStdString(), std::ios_base::out);
+        // if can not create lock, return
+        if (!lock) {
+            qCritical() << "error: could not create lock file.";
+            d->success = false;
+            return;
+        }
+        // write pid into the file
+        lock << getpid() << std::endl;
+        // close file
+        lock.close();
+    }
+
+    LockFile::~LockFile() {
+        if (!d->testing)
+            QFile::remove(d->path);
+        // clean up
+        delete d;
+    }
+
+    bool LockFile::success() const {
+        return d->success;
+    }
+}
--- src/Main.cpp
+++ src/Main.cpp
@@ -20,59 +20,17 @@
 #include "Authenticator.h"
 #include "Configuration.h"
 #include "DisplayManager.h"
+#include "LockFile.h"
 #include "SessionManager.h"
 
-#include <iostream>
-#include <fstream>
-
-#include <unistd.h>
-#include <signal.h>
-
 #include <QApplication>
+#include <QDebug>
 #include <QDeclarativeView>
 #include <QDeclarativeContext>
 
+#include <iostream>
+
 using namespace SDE;
-
-bool createLock(const char *path) {
-    std::ifstream test(path);
-    // check lock file
-    if (test) {
-        // if exists, read pid from it
-        pid_t pid = 0;
-        test >> pid;
-        test.close();
-        // check if another process with the pid is running
-        if (pid > 0) {
-            int ret = kill(pid, 0);
-            if (ret == 0 || errno == EPERM) {
-                // print error message
-                std::cerr << "error: another instance of the program is running with PID " << pid << "." << std::endl;
-                // return fail
-                return false;
-            }
-        }
-        // remove lock file
-        remove(path);
-    }
-    // create lock
-    std::ofstream lock(path, std::ios_base::out);
-    // if can not create lock, return
-    if (!lock) {
-        std::cerr << "error: could not create lock file." << std::endl;
-        return false;
-    }
-    // write pid into the file
-    lock << getpid() << std::endl;
-    // close file
-    lock.close();
-    // return success
-    return true;
-}
-
-void removeLock(const char *path) {
-    remove(path);
-}
 
 int main(int argc, char **argv) {
     QString configPath = "/etc/sddm.conf";
@@ -91,14 +49,14 @@ int main(int argc, char **argv) {
     // create configuration instance
     Configuration configuration(configPath);
 
+    // create lock file
+    LockFile lock(Configuration::instance()->lockFile(), testing);
+    if (!lock.success())
+        return 1;
+
     // set theme
     if (!testing)
         themePath = Configuration::instance()->themesDir() + "/" + Configuration::instance()->currentTheme();
-
-    // create lock, except when in test mode
-    const char *lockFile = Configuration::instance()->lockFile().toStdString().c_str();
-    if ((testing == false) && (createLock(lockFile) == false))
-        return 1;
 
     bool first = true;
 
@@ -111,7 +69,7 @@ int main(int argc, char **argv) {
         displayManager.setCookie(cookie);
         // start the display manager, except when in test mode
         if ((testing == false) && (displayManager.start() == false)) {
-            std::cerr << "error: could not start display manager." << std::endl;
+            qCritical() << "error: could not start display manager.";
             return 1;
         }
         // create session manager
@@ -156,9 +114,6 @@ int main(int argc, char **argv) {
             break;
         }
     }
-    // remove lock
-    if (!testing)
-        removeLock(lockFile);
     // return success
     return 0;
 }
