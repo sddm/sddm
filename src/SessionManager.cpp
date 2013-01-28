d95aa568864d4a63b29495a1be9b053e82254c0a
Abdurrahman AVCI <abdurrahmanavci@gmail.com>
2013-01-28 00:01:22 +0000

    Use QHostInfo to get local host name.
    We depend on QtNetwork through QtQuick anyway.

--- src/SessionManager.cpp
+++ src/SessionManager.cpp
@@ -25,10 +25,9 @@
 #include <QCoreApplication>
 #include <QDir>
 #include <QFile>
+#include <QHostInfo>
 #include <QProcess>
 #include <QTextStream>
-
-#include <unistd.h>
 
 using namespace SDE;
 namespace SDE {
@@ -85,9 +84,7 @@ namespace SDE {
             if (d->sessions.at(i).file == Configuration::instance()->lastSession())
                 d->lastSession = i;
         // get hostname
-        char hostName[100];
-        gethostname(hostName, 100);
-        d->hostName = hostName;
+        d->hostName = QHostInfo::localHostName();
     }
 
     SessionManager::~SessionManager() {
