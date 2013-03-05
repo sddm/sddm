6e3955e3bb67f147e2e08424af12f54440a46852
Abdurrahman AVCI <abdurrahmanavci@gmail.com>
2013-03-05 12:04:47 +0000

    Add missing files.

---
+++ src/common/MessageHandler.h
@@ -0,0 +1,61 @@
+/***************************************************************************
+* Copyright (c) 2013 Abdurrahman AVCI <abdurrahmanavci@gmail.com>
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the
+* Free Software Foundation, Inc.,
+* 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+***************************************************************************/
+
+#include "Configuration.h"
+
+#include <QDateTime>
+#include <QFile>
+
+namespace SDE {
+    void MessageHandler(QtMsgType type, const QMessageLogContext &context, const QString &msg) {
+        Q_UNUSED(context)
+
+        // TODO: log file path should be configurable
+#if !TEST
+        QFile file("/var/log/sddm.log");
+#else
+        QFile file("sddm.log");
+#endif
+
+        // open file
+        if (!file.open(QFile::Append | QFile::WriteOnly))
+            if (!file.open(QFile::Truncate | QFile::WriteOnly))
+                return;
+
+        // create timestamp
+        QString timestamp = QDateTime::currentDateTime().toString("hh:mm:ss.zzz");
+
+        // log message
+        switch (type) {
+            case QtDebugMsg:
+                file.write(QString("[%1] (II) %2\n").arg(timestamp).arg(msg).toLocal8Bit());
+            break;
+            case QtWarningMsg:
+                file.write(QString("[%1] (WW) %2\n").arg(timestamp).arg(msg).toLocal8Bit());
+            break;
+            case QtCriticalMsg:
+            case QtFatalMsg:
+                file.write(QString("[%1] (EE) %2\n").arg(timestamp).arg(msg).toLocal8Bit());
+            break;
+        }
+
+        // close file
+        file.close();
+    }
+}
---
+++ src/common/Messages.h
@@ -0,0 +1,49 @@
+/***************************************************************************
+* Copyright (c) 2013 Abdurrahman AVCI <abdurrahmanavci@gmail.com>
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the
+* Free Software Foundation, Inc.,
+* 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+***************************************************************************/
+
+#ifndef SDE_MESSAGES_H
+#define SDE_MESSAGES_H
+
+namespace SDE {
+    enum class GreeterMessages {
+        Connect = 0,
+        Login,
+        PowerOff,
+        Reboot,
+        Suspend,
+        Hibernate,
+        HybridSleep
+    };
+
+    enum class DaemonMessages {
+        Capabilities,
+        LoginSucceeded,
+        LoginFailed
+    };
+
+    enum class Capabilities {
+        PowerOff = 0x0001,
+        Reboot = 0x0002,
+        Suspend = 0x0004,
+        Hibernate = 0x0008,
+        HybridSleep = 0x0010
+    };
+}
+
+#endif // SDE_MESSAGES_H
