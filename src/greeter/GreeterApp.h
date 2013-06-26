dc3c25c520c439e4e5cfe4eeed6391d8bb78a0a0
Abdurrahman AVCI <abdurrahmanavci@gmail.com>
2013-06-26 13:20:34 +0000

    Fix copyright.

--- src/greeter/GreeterApp.h
+++ src/greeter/GreeterApp.h
@@ -1,5 +1,5 @@
 /***************************************************************************
-* Copyright (c) 2013 Abdurrahman AVCI <abdurrahmanavci@gmail.com>
+* Copyright (c) 2013 Nikita Mikhaylov <nslqqq@gmail.com>
 *
 * This program is free software; you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
