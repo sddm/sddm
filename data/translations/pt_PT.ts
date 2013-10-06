f939d762e1692d1d1ab9d768d82c58cd1b4c45ca
Generator <american.jesus.pt@gmail.com>
2013-10-06 19:59:43 +0100

    Added European Portuguese translation

---
+++ data/translations/pt_PT.ts
@@ -0,0 +1,58 @@
+<!DOCTYPE TS>
+<TS version="2.0" language="pt_PT">
+<context>
+    <name>TextConstants</name>
+    <message>
+        <source>Welcome to %1</source>
+        <translation>Bem-vindo a %1</translation>
+    </message>
+    <message>
+        <source>Warning, Caps Lock is ON!</source>
+        <translation>Atenção, Caps Lock está ligada!</translation>
+    </message>
+    <message>
+        <source>Layout</source>
+        <translation>Disposição</translation>
+    </message>
+    <message>
+        <source>Login</source>
+        <translation>Sessão</translation>
+    </message>
+    <message>
+        <source>Login failed</source>
+        <translation>A autenticação falhou</translation>
+    </message>
+    <message>
+        <source>Login succeeded</source>
+        <translation>Autentificação com sucesso</translation>
+    </message>
+    <message>
+        <source>Password</source>
+        <translation>Senha</translation>
+    </message>
+    <message>
+        <source>Enter your username and password</source>
+        <translation>Escreva o seu nome de utilizador e senha</translation>
+    </message>
+    <message>
+        <source>Reboot</source>
+        <translation>Reiniciar</translation>
+    </message>
+    <message>
+        <source>Session</source>
+        <translation>Sessão</translation>
+    </message>
+    <message>
+        <source>Shutdown</source>
+        <translation>Desligar</translation>
+    </message>
+    <message>
+        <source>User name</source>
+        <translation>Utilizador</translation>
+    </message>
+    <message>
+        <source>Select your user and enter password</source>
+        <translation>Escolha o seu nome de utilizador e escreva a senha</translation>
+    </message>
+</context>
+</TS>
