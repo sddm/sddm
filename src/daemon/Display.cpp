4741f7900a84331325d51f267257587ea75bdbb9
Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
2015-08-21 09:49:36 +0200

    Fix login with UTF-8 user names
    Passing user names to C functions using qPrintable() discards UTF-8
    characters, better use qUtf8Printable() instead.
    
    Closes #365

--- src/daemon/Display.cpp
+++ src/daemon/Display.cpp
@@ -340,7 +340,7 @@ namespace SDDM {
             m_auth->request()->prompts()[0]->setResponse(qPrintable(m_passPhrase));
             m_auth->request()->done();
         } else if (m_auth->request()->prompts().length() == 2) {
-            m_auth->request()->prompts()[0]->setResponse(qPrintable(m_auth->user()));
+            m_auth->request()->prompts()[0]->setResponse(qUtf8Printable(m_auth->user()));
             m_auth->request()->prompts()[1]->setResponse(qPrintable(m_passPhrase));
             m_auth->request()->done();
         }
