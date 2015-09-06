38121dc05b47f33caf53a44e1763f3efcb9e5427
Jeff Huang <s8321414@gmail.com>
2015-09-06 20:06:14 +0800

    Add zh_TW translation.

---
+++ data/translations/zh_TW.ts
@@ -0,0 +1,59 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_TW">
+<context>
+    <name>TextConstants</name>
+    <message>
+        <source>Welcome to %1</source>
+        <translation>歡迎來到 %1</translation>
+    </message>
+    <message>
+        <source>Warning, Caps Lock is ON!</source>
+        <translation>警告，目前 Caps Lock 是開啟的狀態！</translation>
+    </message>
+    <message>
+        <source>Layout</source>
+        <translation>佈局</translation>
+    </message>
+    <message>
+        <source>Login</source>
+        <translation>登入</translation>
+    </message>
+    <message>
+        <source>Login failed</source>
+        <translation>登入失敗</translation>
+    </message>
+    <message>
+        <source>Login succeeded</source>
+        <translation>登入成功</translation>
+    </message>
+    <message>
+        <source>Password</source>
+        <translation>密碼</translation>
+    </message>
+    <message>
+        <source>Enter your username and password</source>
+        <translation>輸入您的使用者名稱與密碼</translation>
+    </message>
+    <message>
+        <source>Reboot</source>
+        <translation>重新開機</translation>
+    </message>
+    <message>
+        <source>Session</source>
+        <translation>工作階段</translation>
+    </message>
+    <message>
+        <source>Shutdown</source>
+        <translation>關機</translation>
+    </message>
+    <message>
+        <source>User name</source>
+        <translation>使用者名稱</translation>
+    </message>
+    <message>
+        <source>Select your user and enter password</source>
+        <translation>選取您的使用者並輸入密碼</translation>
+    </message>
+</context>
+</TS>
