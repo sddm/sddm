b4ebd5e2f1efaa98d98ae1b9f9002841155f0095
Mladen Pejaković <pejakm@autistici.org>
2015-09-08 19:06:43 +0200

    Add Serbian translations (all 4 variants)

---
+++ data/translations/sr@latin.ts
@@ -0,0 +1,59 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="sr@latin">
+<context>
+    <name>TextConstants</name>
+    <message>
+        <source>Welcome to %1</source>
+        <translation>Dobro došli u %1</translation>
+    </message>
+    <message>
+        <source>Warning, Caps Lock is ON!</source>
+        <translation>Upozorenje, uključena su velika slova!</translation>
+    </message>
+    <message>
+        <source>Layout</source>
+        <translation>Raspored</translation>
+    </message>
+    <message>
+        <source>Login</source>
+        <translation>Prijava</translation>
+    </message>
+    <message>
+        <source>Login failed</source>
+        <translation>Prijava nije uspela</translation>
+    </message>
+    <message>
+        <source>Login succeeded</source>
+        <translation>Prijava uspešna</translation>
+    </message>
+    <message>
+        <source>Password</source>
+        <translation>Lozinka</translation>
+    </message>
+    <message>
+        <source>Enter your username and password</source>
+        <translation>Unesite vaše korisničko ime i lozinku</translation>
+    </message>
+    <message>
+        <source>Reboot</source>
+        <translation>Pokreni ponovo</translation>
+    </message>
+    <message>
+        <source>Session</source>
+        <translation>Sesija</translation>
+    </message>
+    <message>
+        <source>Shutdown</source>
+        <translation>Ugasi</translation>
+    </message>
+    <message>
+        <source>User name</source>
+        <translation>Korisničko ime</translation>
+    </message>
+    <message>
+        <source>Select your user and enter password</source>
+        <translation>Izaberite korisnika i unesite lozinku</translation>
+    </message>
+</context>
+</TS>
