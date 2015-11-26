32a43fed52f0af275c53156278a45bcb53623b5b
Laércio de Sousa <laerciosousa@sme-mogidascruzes.sp.gov.br>
2015-11-26 15:13:28 -0200

    Only allocate VTs for seat0
    Currently only systemd-logind special seat named "seat0" can
    handle VTs. Allocating a VT and setting XDG_VTNR for non-seat0
    seats is currently pointless. Moreover, Xorg server 1.16 and
    newer doesn't touch VTs if -seat option is passed with a
    non-seat0 value, so there's no need to pass vt?? argument to
    a non-seat0 Xorg server.
    
    This is a preparation step for multi-seat support. The missing
    parts for a complete support (specially those regarding DBus
    interactions with systemd-logind) will be commited in a second
    moment.
    
    Issue: #78

--- src/daemon/Seat.cpp
+++ src/daemon/Seat.cpp
@@ -54,19 +54,23 @@ namespace SDDM {
     void Seat::createDisplay(int terminalId) {
         //reload config if needed
         mainConfig.load();
-        
-        if (terminalId == -1) {
+
+        if (m_name == QStringLiteral("seat0")) {
+            if (terminalId == -1) {
                 // find unused terminal
-            terminalId = findUnused(mainConfig.XDisplay.MinimumVT.get(), [&](const int number) {
-                return m_terminalIds.contains(number);
-            });
+                terminalId = findUnused(mainConfig.XDisplay.MinimumVT.get(), [&](const int number) {
+                    return m_terminalIds.contains(number);
+                });
+            }
+
+            // mark terminal as used
+            m_terminalIds << terminalId;
+
+            // log message
+            qDebug() << "Adding new display" << "on vt" << terminalId << "...";
+        } else {
+            qDebug() << "Adding new VT-less display...";
         }
-
-        // mark terminal as used
-        m_terminalIds << terminalId;
-
-        // log message
-        qDebug() << "Adding new display" << "on vt" << terminalId << "...";
 
         // create a new display
         Display *display = new Display(terminalId, this);
