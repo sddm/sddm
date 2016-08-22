25d71f2ed5d5d5b66eddcf49fee4c92c532ee4e4
Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
2016-08-22 20:44:29 +0200

    helper: Delete credentials after closing the session
    The credentials should be deleted after the session has
    been closed with pam_close_session.

--- src/helper/backend/PamBackend.cpp
+++ src/helper/backend/PamBackend.cpp
@@ -279,7 +279,9 @@ namespace SDDM {
     bool PamBackend::closeSession() {
         if (m_pam->isOpen()) {
             qDebug() << "[PAM] Closing session";
-            return m_pam->closeSession();
+            m_pam->closeSession();
+            m_pam->setCred(PAM_DELETE_CRED);
+            return true;
         }
         qWarning() << "[PAM] Asked to close the session but it wasn't previously open";
         return Backend::closeSession();
