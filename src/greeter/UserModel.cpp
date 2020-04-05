4b9c228b83d1e76270fb43f63ddd8846fcce26d2
Giuseppe Calà <jiveaxe@gmail.com>
2020-04-05 10:53:31 +0200

    Use avatars in FacesDir first and if not found search other locations
    If Avatars are enabled and home is encrypted it takes 60s to popup the greeter at boot. The same is if copying $USER.face.icon in FacesDir since avatar is searched first in home.
    
    The patch changes this logic. Now it search first in FacesDir and if found the greeter is shown, otherwise queries other default locations.

--- src/greeter/UserModel.cpp
+++ src/greeter/UserModel.cpp
@@ -134,16 +134,20 @@ namespace SDDM {
                 d->lastIndex = i;
 
             if (avatarsEnabled) {
-                const QString userFace = QStringLiteral("%1/.face.icon").arg(user->homeDir);
                 const QString systemFace = QStringLiteral("%1/%2.face.icon").arg(facesDir).arg(user->name);
-                QString accountsServiceFace = QStringLiteral("/var/lib/AccountsService/icons/%1").arg(user->name);
-
-                if (QFile::exists(userFace))
-                    user->icon = QStringLiteral("file://%1").arg(userFace);
-                else if (QFile::exists(accountsServiceFace))
-                    user->icon = accountsServiceFace;
-                else if (QFile::exists(systemFace))
+                 
+                if (QFile::exists(systemFace))  {
                     user->icon = QStringLiteral("file://%1").arg(systemFace);
+                } else {
+                    const QString userFace = QStringLiteral("%1/.face.icon").arg(user->homeDir);
+                    QString accountsServiceFace = QStringLiteral("/var/lib/AccountsService/icons/%1").arg(user->name);
+
+                    if (QFile::exists(userFace)) {
+                        user->icon = QStringLiteral("file://%1").arg(userFace);
+                    } else if (QFile::exists(accountsServiceFace)) {
+                        user->icon = accountsServiceFace;
+                    }
+                 }
             }
         }
     }
