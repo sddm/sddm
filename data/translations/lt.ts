34b218e8a379f4af2dd3aa38e9d0dc0314b26d36
welaq <welaq@users.noreply.github.com>
2020-11-07 21:47:21 +0200

    Updated Lithuanian translation

--- data/translations/lt.ts
+++ data/translations/lt.ts
@@ -2,6 +2,20 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="lt">
 <context>
+    <name>PictureBox</name>
+    <message>
+        <source>Press to login</source>
+        <translation>Paspauskite, norėdami prisijungti</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <source>%1 (Wayland)</source>
+        <translation>%1 (Wayland)</translation>
+    </message>
+</context>
+<context>
     <name>TextConstants</name>
     <message>
         <source>Welcome to %1</source>
@@ -9,11 +23,11 @@
     </message>
     <message>
         <source>Warning, Caps Lock is ON!</source>
-        <translation>Dėmesio, įjungtos Didžiosios raidės!</translation>
+        <translation>Įspėjimas, įjungtos Didžiosios raidės!</translation>
     </message>
     <message>
         <source>Layout</source>
-        <translation>Atsijungti</translation>
+        <translation>Išdėstymas</translation>
     </message>
     <message>
         <source>Login</source>
@@ -40,20 +54,64 @@
         <translation>Paleisti iš naujo</translation>
     </message>
     <message>
+        <source>Suspend</source>
+        <translation>Pristabdyti</translation>
+    </message>
+    <message>
+        <source>Hibernate</source>
+        <translation>Užmigdyti</translation>
+    </message>
+    <message>
         <source>Session</source>
-        <translation>Sesija</translation>
+        <translation>Seansas</translation>
     </message>
     <message>
         <source>Shutdown</source>
         <translation>Išjungti</translation>
     </message>
     <message>
-        <source>User name</source>
+        <source>Username</source>
         <translation>Naudotojo vardas</translation>
+    </message>
+    <message>
+        <source>Enter your username</source>
+        <translation>Įveskite savo naudotojo vardą</translation>
+    </message>
+    <message>
+        <source>Enter your password</source>
+        <translation>Įveskite savo slaptažodį</translation>
     </message>
     <message>
         <source>Select your user and enter password</source>
         <translation>Pasirinkite savo naudotoją ir įveskite slaptažodį</translation>
+    </message>
+    <message>
+        <source>Show password</source>
+        <translation>Rodyti slaptažodį</translation>
+    </message>
+    <message>
+        <source>Hide password</source>
+        <translation>Slėpti slaptažodį</translation>
+    </message>
+    <message>
+        <source>Please enter a password!</source>
+        <translation>Įveskite slaptažodį!</translation>
+    </message>
+    <message>
+        <source>Change password</source>
+        <translation>Keisti slaptažodį</translation>
+    </message>
+    <message>
+        <source>Password:</source>
+        <translation>Slaptažodis:</translation>
+    </message>
+    <message>
+        <source>Password change aborted because maximum tries reached</source>
+        <translation>Slaptažodžio keitimas nutrauktas, nes pasiektas didžiausias bandymų skaičius</translation>
+    </message>
+    <message>
+        <source>New password change round! Please input current password again!</source>
+        <translation>Dar kartą įveskite dabartinį slaptažodį!</translation>
     </message>
 </context>
 </TS>
