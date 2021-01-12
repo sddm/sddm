fc6b76bace8b313b6b304b31297ba738fa7a2dee
Giuseppe Calà <jiveaxe@gmail.com>
2021-01-12 10:15:09 +0100

    less convoluted code

--- src/greeter/UserModel.cpp
+++ src/greeter/UserModel.cpp
@@ -135,19 +135,16 @@ namespace SDDM {
 
             if (avatarsEnabled) {
                 const QString systemFace = QStringLiteral("%1/%2.face.icon").arg(facesDir).arg(user->name);
-                 
-                if (QFile::exists(systemFace))  {
+                const QString userFace = QStringLiteral("%1/.face.icon").arg(user->homeDir);
+                const QString accountsServiceFace = QStringLiteral("/var/lib/AccountsService/icons/%1").arg(user->name);
+                
+                if (QFile::exists(systemFace)) {
                     user->icon = QStringLiteral("file://%1").arg(systemFace);
-                } else {
-                    const QString userFace = QStringLiteral("%1/.face.icon").arg(user->homeDir);
-                    QString accountsServiceFace = QStringLiteral("/var/lib/AccountsService/icons/%1").arg(user->name);
-
-                    if (QFile::exists(userFace)) {
-                        user->icon = QStringLiteral("file://%1").arg(userFace);
-                    } else if (QFile::exists(accountsServiceFace)) {
-                        user->icon = accountsServiceFace;
-                    }
-                 }
+                } else if (QFile::exists(userFace)) {
+                    user->icon = QStringLiteral("file://%1").arg(userFace);
+                } else if (QFile::exists(accountsServiceFace)) {
+                    user->icon = accountsServiceFace;
+                }
             }
         }
     }
