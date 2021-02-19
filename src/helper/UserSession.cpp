32e69094a9213dddf6d1ca7ce2bb2b38ad83eb88
Adriaan de Groot <groot@kde.org>
2021-02-20 00:08:06 +0100

    FreeBSD: use login capabilities

--- src/helper/UserSession.cpp
+++ src/helper/UserSession.cpp
@@ -34,6 +34,10 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <sched.h>
+#if defined(Q_OS_FREEBSD)
+/* Login capabilities database, see UserSession::start()  */
+#include <login_cap.h>
+#endif
 
 namespace SDDM {
     UserSession::UserSession(HelperApp *parent)
@@ -139,6 +143,21 @@ namespace SDDM {
 
         // switch user
         const QByteArray username = qobject_cast<HelperApp*>(parent())->user().toLocal8Bit();
+#ifdef Q_OS_FREEBSD
+        struct passwd *pw = getpwnam(username.constData());
+        /* execve() uses the environment prepared in Backend::openSession(),
+           therefore environment variables which are set here are ignored. */
+        if (setusercontext(NULL, pw, pw->pw_uid, LOGIN_SETALL) != 0) {
+            qCritical() << "setusercontext(NULL, *, " << pw->pw_uid << ", LOGIN_SETALL) failed for user: " << username;
+            exit(Auth::HELPER_OTHER_ERROR);
+        }
+        if (chdir(pw->pw_dir) != 0) {
+            qCritical() << "chdir(" << pw->pw_dir << ") failed for user: " << username;
+            qCritical() << "verify directory exist and has sufficient permissions";
+            exit(Auth::HELPER_OTHER_ERROR);
+        }
+        const QString homeDir = QString::fromLocal8Bit(pw->pw_dir);
+#else        
         struct passwd pw;
         struct passwd *rpw;
         long bufsize = sysconf(_SC_GETPW_R_SIZE_MAX);
@@ -230,6 +249,7 @@ namespace SDDM {
             exit(Auth::HELPER_OTHER_ERROR);
         }
         const QString homeDir = QString::fromLocal8Bit(pw.pw_dir);
+#endif
 
         //we cannot use setStandardError file as this code is run in the child process
         //we want to redirect after we setuid so that the log file is owned by the user
