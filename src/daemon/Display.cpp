5a81948f11549e52e29e2aa39b41f4dea22190a7
Adriaan de Groot <groot@kde.org>
2021-02-19 23:11:53 +0100

    FreeBSD: only set *_DESKTOP if non-empty
    Only set XDG_CURRENT_DESKTOP and XDG_SESSION_DESKTOP if the xsession .desktop
    file defines DesktopNames. FreeBSD xinitrc.desktop does not set it because that
    may confuse the desktop session started from ~/.xinitrc.  These environment
    variables are used for OnlyShowIn and NotShowIn in .desktop files.
    
    Perhaps these two env.insert() calls can be collected in one if() but that
    does change the order in which the variables are inserted, so it has been
    left that way.

--- src/daemon/Display.cpp
+++ src/daemon/Display.cpp
@@ -320,11 +320,13 @@ namespace SDDM {
         env.insert(QStringLiteral("XDG_SEAT_PATH"), daemonApp->displayManager()->seatPath(seat()->name()));
         env.insert(QStringLiteral("XDG_SESSION_PATH"), daemonApp->displayManager()->sessionPath(QStringLiteral("Session%1").arg(daemonApp->newSessionId())));
         env.insert(QStringLiteral("DESKTOP_SESSION"), session.desktopSession());
-        env.insert(QStringLiteral("XDG_CURRENT_DESKTOP"), session.desktopNames());
+        if (!session.desktopNames().isEmpty())
+            env.insert(QStringLiteral("XDG_CURRENT_DESKTOP"), session.desktopNames());
         env.insert(QStringLiteral("XDG_SESSION_CLASS"), QStringLiteral("user"));
         env.insert(QStringLiteral("XDG_SESSION_TYPE"), session.xdgSessionType());
         env.insert(QStringLiteral("XDG_SEAT"), seat()->name());
-        env.insert(QStringLiteral("XDG_SESSION_DESKTOP"), session.desktopNames());
+        if (!session.desktopNames().isEmpty())
+            env.insert(QStringLiteral("XDG_SESSION_DESKTOP"), session.desktopNames());
 
         m_auth->insertEnvironment(env);
 
