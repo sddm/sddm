badeaee3ddaaf61fd03cdd09bc138dadd88919c0
Pier Luigi Fiorini <pierluigi.fiorini@liri.io>
2021-02-28 12:07:33 +0100

    Allocate VT for the display
    Replace the old crude algorithm to find the next available VT with a
    more reliable method using the VT_OPENQRY ioctl.
    
    General.MinimumVT setting is now obsolete and it's no longer used.

--- src/daemon/Display.cpp
+++ src/daemon/Display.cpp
@@ -45,16 +45,18 @@
 
 #include "Login1Manager.h"
 #include "Login1Session.h"
-
+#include "VirtualTerminal.h"
 
 namespace SDDM {
-    Display::Display(const int terminalId, Seat *parent) : QObject(parent),
-        m_terminalId(terminalId),
+    Display::Display(Seat *parent) : QObject(parent),
         m_auth(new Auth(this)),
         m_displayServer(new XorgDisplayServer(this)),
         m_seat(parent),
         m_socketServer(new SocketServer(this)),
         m_greeter(new Greeter(this)) {
+
+        // Allocate vt
+        m_terminalId = VirtualTerminal::setUpNewVt();
 
         // respond to authentication requests
         m_auth->setVerbose(true);
