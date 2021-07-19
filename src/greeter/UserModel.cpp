sddm/sddm

commit d34be784b2c3c018628c4f7a0911969fb9b6ef89

Use avatars in FacesDir first and if not found search other locations
If Avatars are enabled and home is encrypted it takes 60s to popup
the greeter at boot. The same is if copying $USER.face.icon in FacesDir
since avatar is searched first in home.

The patch changes this logic. Now it search first in FacesDir and if
found the greeter is shown, otherwise queries other default locations.

src/greeter/UserModel.cpp
@@ -136,14 +136,16 @@ namespace SDDM {
             if (avatarsEnabled) {
                 const QString userFace = QStringLiteral("%1/.face.icon").arg(user->homeDir);
                 const QString systemFace = QStringLiteral("%1/%2.face.icon").arg(facesDir).arg(user->name);
-                QString accountsServiceFace = QStringLiteral("/var/lib/AccountsService/icons/%1").arg(user->name);
+                const QString accountsServiceFace = QStringLiteral("/var/lib/AccountsService/icons/%1").arg(user->name);
 
-                if (QFile::exists(userFace))
-                    user->icon = QStringLiteral("file://%1").arg(userFace);
+                // If the home is encrypted it takes a lot of time to open
+                // up the greeter, therefore we try the system avatar first
+                if (QFile::exists(systemFace))
+                    user->icon = systemFace;
+                else if (QFile::exists(userFace))
+                    user->icon = userFace;
                 else if (QFile::exists(accountsServiceFace))
                     user->icon = accountsServiceFace;
-                else if (QFile::exists(systemFace))
-                    user->icon = QStringLiteral("file://%1").arg(systemFace);
             }
         }
     }

commit 762e5c15c2318fe0e49794c5d28f1f2ff923796b

Fix displaying user icons
Regression from d34be784b2

src/greeter/UserModel.cpp
@@ -138,14 +138,18 @@ namespace SDDM {
                 const QString systemFace = QStringLiteral("%1/%2.face.icon").arg(facesDir).arg(user->name);
                 const QString accountsServiceFace = QStringLiteral("/var/lib/AccountsService/icons/%1").arg(user->name);
 
+                QString userIcon;
                 // If the home is encrypted it takes a lot of time to open
                 // up the greeter, therefore we try the system avatar first
                 if (QFile::exists(systemFace))
-                    user->icon = systemFace;
+                    userIcon = systemFace;
                 else if (QFile::exists(userFace))
-                    user->icon = userFace;
+                    userIcon = userFace;
                 else if (QFile::exists(accountsServiceFace))
-                    user->icon = accountsServiceFace;
+                    userIcon = accountsServiceFace;
+
+                if (!userIcon.isEmpty())
+                    user->icon = QStringLiteral("file://%1").arg(userIcon);
             }
         }
     }