19ef5d589b695a7758625cd067888031503b0423
Gleb Popov <6yearold@gmail.com>
2021-10-03 16:33:29 +0400

    Only set XDG_CURRENT_DESKTOP env var if it is defined in the session's .desktop file.
    Only set XDG_SESSION_DESKTOP if we HAVE_SYSTEMD, as this is a systemd-specific
    env variable.
    
    FreeBSD xinitrc.desktop does not set XDG_CURRENT_DESKTOP because that may
    confuse the desktop session started from ~/.xinitrc.  These environment variables
    are used for OnlyShowIn and NotShowIn in .desktop files.

--- src/daemon/Display.cpp
+++ src/daemon/Display.cpp
@@ -381,12 +381,15 @@ namespace SDDM {
         env.insert(QStringLiteral("XDG_SEAT_PATH"), daemonApp->displayManager()->seatPath(seat()->name()));
         env.insert(QStringLiteral("XDG_SESSION_PATH"), daemonApp->displayManager()->sessionPath(QStringLiteral("Session%1").arg(daemonApp->newSessionId())));
         env.insert(QStringLiteral("DESKTOP_SESSION"), session.desktopSession());
-        env.insert(QStringLiteral("XDG_CURRENT_DESKTOP"), session.desktopNames());
+        if (!session.desktopNames().isEmpty())
+            env.insert(QStringLiteral("XDG_CURRENT_DESKTOP"), session.desktopNames());
         env.insert(QStringLiteral("XDG_SESSION_CLASS"), QStringLiteral("user"));
         env.insert(QStringLiteral("XDG_SESSION_TYPE"), session.xdgSessionType());
         env.insert(QStringLiteral("XDG_SEAT"), seat()->name());
         env.insert(QStringLiteral("XDG_VTNR"), QString::number(m_lastSession.vt()));
+#ifdef HAVE_SYSTEMD
         env.insert(QStringLiteral("XDG_SESSION_DESKTOP"), session.desktopNames());
+#endif
 
         if (session.xdgSessionType() == QLatin1String("x11")) {
           if (m_displayServerType == X11DisplayServerType)
