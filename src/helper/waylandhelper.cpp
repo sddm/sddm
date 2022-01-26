82c6e6d086d3ebc733a354d9dd5d438c8697926e
Aleix Pol <aleixpol@kde.org>
2022-01-27 00:39:23 +0100

    WaylandHelper: Also terminate the greeter as we wrap up

--- src/helper/waylandhelper.cpp
+++ src/helper/waylandhelper.cpp
@@ -55,17 +55,23 @@ bool WaylandHelper::startCompositor(const QString &cmd)
     return startProcess(cmd, &m_serverProcess);
 }
 
+void stopProcess(QProcess *process)
+{
+    if (process) {
+        qInfo() << "Stopping..." << process->program();
+        process->terminate();
+        if (!process->waitForFinished(5000))
+            process->kill();
+        process->deleteLater();
+        process = nullptr;
+    }
+}
+
 void WaylandHelper::stop()
 {
     m_watcher->stop();
-    if (m_serverProcess) {
-        qInfo("Stopping server...");
-        m_serverProcess->terminate();
-        if (!m_serverProcess->waitForFinished(5000))
-            m_serverProcess->kill();
-        m_serverProcess->deleteLater();
-        m_serverProcess = nullptr;
-    }
+    stopProcess(m_greeterProcess);
+    stopProcess(m_serverProcess);
 }
 
 bool WaylandHelper::startProcess(const QString &cmd, QProcess **p)
@@ -108,30 +114,26 @@ void WaylandHelper::startGreeter(const QString &cmd)
 {
     auto args = QProcess::splitCommand(cmd);
 
-    QProcess *process = new QProcess(this);
-    process->setProgram(args.takeFirst());
-    process->setArguments(args);
-    process->setProcessEnvironment(m_environment);
-    connect(process, &QProcess::readyReadStandardError, this, [process] {
-        qWarning() << process->readAllStandardError();
+    m_greeterProcess = new QProcess(this);
+    m_greeterProcess->setProgram(args.takeFirst());
+    m_greeterProcess->setArguments(args);
+    m_greeterProcess->setProcessEnvironment(m_environment);
+    connect(m_greeterProcess, &QProcess::readyReadStandardError, this, [this] {
+        qWarning() << m_greeterProcess->readAllStandardError();
     });
-    connect(process, &QProcess::readyReadStandardOutput, this, [process] {
-        qInfo() << process->readAllStandardOutput();
+    connect(m_greeterProcess, &QProcess::readyReadStandardOutput, this, [this] {
+        qInfo() << m_greeterProcess->readAllStandardOutput();
     });
-    startGreeter(process);
-}
 
-void WaylandHelper::startGreeter(QProcess *process)
-{
     if (m_watcher->status() == WaylandSocketWatcher::Started) {
-        process->start();
+        m_greeterProcess->start();
     } else if (m_watcher->status() == WaylandSocketWatcher::Failed) {
         Q_EMIT failed();
     } else {
         connect(m_watcher, &WaylandSocketWatcher::failed, this, &WaylandHelper::failed);
-        connect(m_watcher, &WaylandSocketWatcher::started, this, [this, process] {
+        connect(m_watcher, &WaylandSocketWatcher::started, this, [this] {
             m_watcher->stop();
-            process->start();
+            m_greeterProcess->start();
         });
     }
 }
