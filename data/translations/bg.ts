51741bf105303542234fe9c2f9e34b9b9432a9fe
Salif Mehmed <mail@salif.eu>
2023-08-16 14:05:41 +0300

    Add Bulgarian translation

---
+++ data/translations/bg.ts
@@ -0,0 +1,117 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="bg">
+<context>
+    <name>PictureBox</name>
+    <message>
+        <source>Press to login</source>
+        <translation>Натиснете, за да влезете</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <source>%1 (Wayland)</source>
+        <translation>%1 (Wayland)</translation>
+    </message>
+</context>
+<context>
+    <name>TextConstants</name>
+    <message>
+        <source>Welcome to %1</source>
+        <translation>Добре дошли в %1</translation>
+    </message>
+    <message>
+        <source>Warning, Caps Lock is ON!</source>
+        <translation>Внимание, Caps Lock е включен!</translation>
+    </message>
+    <message>
+        <source>Layout</source>
+        <translation>Клавиатура</translation>
+    </message>
+    <message>
+        <source>Login</source>
+        <translation>Влизане</translation>
+    </message>
+    <message>
+        <source>Login failed</source>
+        <translation>Неуспешно влизане</translation>
+    </message>
+    <message>
+        <source>Login succeeded</source>
+        <translation>Успешно влизане</translation>
+    </message>
+    <message>
+        <source>Password</source>
+        <translation>Парола</translation>
+    </message>
+    <message>
+        <source>Enter your username and password</source>
+        <translation>Въведете потребителско име и парола</translation>
+    </message>
+    <message>
+        <source>Reboot</source>
+        <translation>Рестартиране</translation>
+    </message>
+        <message>
+        <source>Suspend</source>
+        <translation>Приспиване</translation>
+    </message>
+        <message>
+        <source>Hibernate</source>
+        <translation>Дълбоко заспиване</translation>
+    </message>
+    <message>
+        <source>Session</source>
+        <translation>Сесия</translation>
+    </message>
+    <message>
+        <source>Shutdown</source>
+        <translation>Изключване</translation>
+    </message>
+    <message>
+        <source>Username</source>
+        <translation>Потребителско име</translation>
+    </message>
+        <message>
+        <source>Enter your username</source>
+        <translation>Въведете потребителското си име</translation>
+    </message>
+    <message>
+        <source>Enter your password</source>
+        <translation>Въведете паролата си</translation>
+    </message>
+    <message>
+        <source>Select your user and enter password</source>
+        <translation>Изберете потребител и въведете парола</translation>
+    </message>
+     <message>
+        <source>Show password</source>
+        <translation>Показване на парола</translation>
+    </message>
+    <message>
+        <source>Hide password</source>
+        <translation>Скриване на парола</translation>
+    </message>
+    <message>
+        <source>Please enter a password!</source>
+        <translation>Моля, въведете парола!</translation>
+    </message>
+    <message>
+        <source>Change password</source>
+        <translation>Промяна на парола</translation>
+    </message>
+    <message>
+        <source>Password:</source>
+        <translation>Парола:</translation>
+    </message>
+    <message>
+        <source>Password change aborted because maximum tries reached</source>
+        <translation>Промяната на паролата е прекратена, тъй като е достигнат максималния брой опити</translation>
+    </message>
+    <message>
+        <source>New password change round! Please input current password again!</source>
+        <translation>Нов рунд за промяна на паролата! Моля, въведете текущата парола отново!</translation>
+    </message>
+</context>
+</TS>
