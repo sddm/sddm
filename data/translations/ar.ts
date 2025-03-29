3882f234e14b701d3f09cdc3edb554f2d5fd936d
Mohamed A. Abdallah <Eng.Mohamed.A.Abdallah@gmail.com>
2025-03-29 03:06:15 +0200

    Add missing message tags and it's corresponding translations

--- data/translations/ar.ts
+++ data/translations/ar.ts
@@ -11,6 +11,10 @@
 <context>
     <name>QObject</name>
     <message>
+        <source>%1</source>
+        <translation>%1</translation>
+    </message>
+    <message>
         <source>%1 (Wayland)</source>
         <translation>%1 (Wayland)</translation>
     </message>
@@ -62,12 +66,52 @@
         <translation>أيقاف التشغيل</translation>
     </message>
     <message>
+    <source>Suspend</source>
+        <translation>ايقاف</translation>
+    </message>
+    <message>
+        <source>Hibernate</source>
+        <translation>وضع الثبات</translation>
+    </message>
+    <message>
         <source>Username</source>
         <translation>اسم المستخدم</translation>
     </message>
     <message>
         <source>Select your user and enter password</source>
         <translation>اختر مستخدمًا وأدخل كلمة السر</translation>
+    </message>
+    <message>
+        <source>Please enter a password!</source>
+        <translation>برجاء ادخال كلمة السر</translation>
+    </message>
+    <message>
+        <source>Change password</source>
+        <translation>تغيير كلمة السر</translation>
+    </message>
+    <message>
+        <source>Enter your username</source>
+        <translation>ادخل اسم المستخدم الخاص بك</translation>
+    </message>
+    <message>
+        <source>Enter your password</source>
+        <translation>ادخل كلمة السر الخاصة بك</translation>
+    </message>
+    <message>
+        <source>Password:</source>
+        <translation>كلمة السر:</translation>
+    </message>
+    <message>
+        <source>Show password</source>
+        <translation>اظهر كلمة السر</translation>
+    </message>
+    <message>
+        <source>Hide password</source>
+        <translation>اخفِ كلمة السر</translation>
+    </message>
+    <message>
+        <source>Password change aborted because maximum tries reached</source>
+        <translation>لا يمكن تغيير كلمة السر لتخطي اقصي عدد ممكن من المحاولات</translation>
     </message>
 </context>
 </TS>
