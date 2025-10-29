ae0b54bb8789d6cf8e1d93baa20cddf0265cda6d
sgakerru <sulmpx60@yandex.ru>
2025-10-29 23:41:20 +0400

    Auth: do not manually read the '/etc/locale.conf' if we have systemd
    Also, we should unquote values before inserting them in env for non-systemd case, otherwise locale params will not be valid in the child process.

--- src/auth/Auth.cpp
+++ src/auth/Auth.cpp
@@ -114,6 +114,7 @@ namespace SDDM {
             , child(new QProcess(this))
             , id(lastId++) {
         SocketServer::instance()->helpers[id] = this;
+#ifndef HAVE_SYSTEMD
         QProcessEnvironment env = child->processEnvironment();
         bool langEmpty = true;
         QFile localeFile(QStringLiteral("/etc/locale.conf"));
@@ -122,6 +123,7 @@ namespace SDDM {
             while (!in.atEnd()) {
                 QStringList parts = in.readLine().split(QLatin1Char('='));
                 if (parts.size() >= 2) {
+                    // TODO: value (parts[1]) has to be unquoted before inserting in env
                     env.insert(parts[0], parts[1]);
                     if (parts[0] == QLatin1String("LANG"))
                         langEmpty = false;
@@ -132,6 +134,7 @@ namespace SDDM {
         if (langEmpty)
             env.insert(QStringLiteral("LANG"), QStringLiteral("C"));
         child->setProcessEnvironment(env);
+#endif
         connect(child, QOverload<int,QProcess::ExitStatus>::of(&QProcess::finished), this, &Auth::Private::childExited);
         connect(child, &QProcess::errorOccurred, this, &Auth::Private::childError);
         connect(request, &AuthRequest::finished, this, &Auth::Private::requestFinished);
