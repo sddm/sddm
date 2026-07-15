79153e3396521c3004c3fc7c696b506a51f418da
Herman Semenoff <GermanAizek@yandex.ru>
2026-07-15 14:13:40 +0300

    Code simplify using const ref var and separate var

--- src/daemon/Display.cpp
+++ src/daemon/Display.cpp
@@ -549,13 +549,15 @@ namespace SDDM {
     }
 
     void Display::slotRequestChanged() {
-        if (m_auth->request()->prompts().length() == 1) {
-            m_auth->request()->prompts()[0]->setResponse(qPrintable(m_passPhrase));
-            m_auth->request()->done();
-        } else if (m_auth->request()->prompts().length() == 2) {
-            m_auth->request()->prompts()[0]->setResponse(qPrintable(m_auth->user()));
-            m_auth->request()->prompts()[1]->setResponse(qPrintable(m_passPhrase));
-            m_auth->request()->done();
+        Request *currentRequest = m_auth->request();
+
+        if (currentRequest->prompts().length() == 1) {
+            currentRequest->prompts()[0]->setResponse(qPrintable(m_passPhrase));
+            currentRequest->done();
+        } else if (currentRequest->prompts().length() == 2) {
+            currentRequest->prompts()[0]->setResponse(qPrintable(m_auth->user()));
+            currentRequest->prompts()[1]->setResponse(qPrintable(m_passPhrase));
+            currentRequest->done();
         }
     }
 
